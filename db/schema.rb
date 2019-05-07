1ea208095dde29e92cdc9a2a412c45d5ce65aa6e
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-07 21:27:56 +1000

    Add user_id foreing key to reservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_064212) do
+ActiveRecord::Schema.define(version: 2019_05_07_112220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,12 +33,13 @@ ActiveRecord::Schema.define(version: 2019_05_07_064212) do
   end
 
   create_table "reservations", force: :cascade do |t|
-    t.integer "user_id"
     t.integer "flight_id"
     t.integer "row"
     t.string "column"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +50,5 @@ ActiveRecord::Schema.define(version: 2019_05_07_064212) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "users"
 end
