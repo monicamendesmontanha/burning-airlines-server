3bf3c09fe2eac93973c420c580b41a9e09b0adf4
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-08 15:04:11 +1000

    Add session / Login & Logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to new_user_url
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       # Handle a successful save.
       flash[:success] = "Welcome to Burning Airlines!"
       redirect_to @user
