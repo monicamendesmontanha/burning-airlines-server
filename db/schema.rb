c75e8ff1666aab3e5eb6582bbcab55f37d90b51c
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-08 12:30:49 +1000

    Add index to users email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_022808) do
+ActiveRecord::Schema.define(version: 2019_05_08_022934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_05_08_022808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "flights", "airplanes"
