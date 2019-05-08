73fca1c3edd9cd886262544a0cc9e33f2f7c22ac
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-08 14:32:07 +1000

    Add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   resources :reservations
-  resources :users
   resources :flights
   resources :airplanes
+  resources :users
+
+  get    '/signup',  to: 'users#new'
+
+  # get 'sessions/new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
