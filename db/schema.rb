801fb7d5f2d03a893ac157003fdf84e6fca3648d
Monica Mendes <monicamendesmontanha@gmail.com>
2019-05-08 12:38:23 +1000

    Add password digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_022934) do
+ActiveRecord::Schema.define(version: 2019_05_08_023716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_05_08_022934) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
